3b735ddedea24ef668fd4d4bc8eabc8e93c8940e
Fabi Yamada <33674731+fabiyamada@users.noreply.github.com>
2025-09-03 21:42:02 -0700

    Updated ClipboardStorage.jsx

--- src/components/ClipboardStorage.jsx
+++ src/components/ClipboardStorage.jsx
@@ -40,7 +40,7 @@ function ClipboardStorage() {
   };
 
   return (
-    <div className="space-y-2 p-4 border rounded">
+    <div className="space-y-2 p-4 border rounded flex justify">
       <button
         onClick={handleCopyFromLocalStorage}
         className="px-3 py-2 rounded-xl bg-blue-500 text-white mr-3"
